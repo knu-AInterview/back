9047c8db119dfb88f32fc7049f6f58a7222acb6a
ssuu7861 <ssuu7861@gmail.com>
2023-11-25 19:51:15 +0900

    init project

---
+++ src/main/java/knu/ainterview/AinterviewApplication.java
@@ -0,0 +1,13 @@
+package knu.ainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knu/ainterview/AinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.ainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
